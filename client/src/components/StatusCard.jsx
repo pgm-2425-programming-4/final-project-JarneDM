2ac555cf99e185f77496c86b71f619adfd70ded7
Jarne <jarne.demeyer05@hotmail.com>
2025-06-09 19:07:16 +0200

    rendering status titles and styling them a little bit

--- client/src/components/StatusCard.jsx
+++ client/src/components/StatusCard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/Tasks.css";
 
-function StatusCard({ tasks, loading, error }) {
+function StatusCard() {
+  const [status, setStatus] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // http://localhost:1337/api/statuses?populate[tasks]=true
+
+  useEffect(() => {
+    const fetchStatuses = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`http://localhost:1337/api/statuses?populate[tasks]=true`);
+        const data = await res.json();
+
+        if (data?.data) {
+          setStatus(data.data);
+        } else {
+          setError("Unexpected data format");
+        }
+      } catch (err) {
+        console.error("Failed to fetch tasks:", err);
+        setError("Failed to fetch tasks");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStatuses();
+  }, []);
   if (loading) {
     return <div>Loading task statuses...</div>;
   }
@@ -11,9 +39,13 @@ function StatusCard({ tasks, loading, error }) {
   }
 
   return (
-    <div className="statusTitle">
-      {tasks?.map((task) => (
-        <div key={task.id}>{task.taskStatus?.name || "No status"}</div>
+    <div className="status-container">
+      {status?.map((stat) => (
+        <div key={stat.id} className="status-card">
+          <div className="status__title" key={stat.id}>
+            {stat.name || " No status"}
+          </div>
+        </div>
       ))}
     </div>
   );
