04227846f90e4dbefc658d05ac6e73cd6896a115
Jarne <jarne.demeyer05@hotmail.com>
2025-06-17 18:36:57 +0200

    add about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,3 +1,35 @@
+import "./css/About.css";
+
 export default function About() {
-  return <div>About this app</div>;
+  return (
+    <>
+      <div className="about-container">
+        <h1>Over dit project</h1>
+        <p>
+          Dit project is ontstaan als een onderdeel van mijn studie aan Hogeschool Artevelde. Het is een webtool die het beheer van
+          projecten vergemakkelijkt door een duidelijk overzicht te bieden van taken, labels en statussen. De applicatie is ontwikkeld met
+          React voor de front-end en maakt gebruik van een headless CMS (Strapi) voor de back-end. De gebruiksvriendelijke interface stelt
+          gebruikers in staat om snel en effectief nieuwe taken te creëren en te beheren.
+        </p>
+
+        <div className="contact">
+          <h2>Contactgegevens</h2>
+          <ul>
+            <li>
+              <strong>Naam:</strong> Jarne De Meyer
+            </li>
+            <li>
+              <strong>E-mail:</strong> <a href="mailto:jarne.demeyer@student.arteveldehs.be">jarne.demeyer@student.arteveldehs.be</a>
+            </li>
+            <li>
+              <strong>LinkedIn:</strong>{" "}
+              <a href="https://www.linkedin.com/in/jarne-de-meyer-22a2b5358/" target="_blank" rel="noopener noreferrer">
+                linkedin.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
